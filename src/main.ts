f11315f74ed11ec8088cc3f404ebea9ebca4ae5e
nubidebs <debora.crescenzo@truepill.com>
2022-04-03 15:55:12 +0100

    create and save user

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // to add validations
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Additional properties sent in the body of POST requests are stripped out
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { create } from 'domain';
+import { stringify } from 'querystring';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  /* repo: argument name.
+    Type annotation of repo is Repository, we applied a generic type to it of User: which means that
+    repos is going to be an instance of a TypeORM repository that deals with instances of users
+    @InjectRepository: aid to the dep injection system that tells to inject the generic User 
+    (used because we are using the generic type of User)*/
+
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+  create(email: string, password: string) {
+    const user = this.repo.create({ email, password });
+
+    return this.repo.save(user);
+  }
+}
