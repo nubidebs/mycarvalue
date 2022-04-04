04a88d9f46ce711c0da31f7d0ff4284ccf1d6240
nubidebs <debora.crescenzo@truepill.com>
2022-04-04 17:05:47 +0100

    hash and salt pw

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,6 +1,9 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
 
+const scrypt = promisify(_scrypt);
 @Injectable()
 export class AuthService {
   constructor(private userService: UsersService) {}
@@ -12,11 +15,16 @@ export class AuthService {
       throw new BadRequestException('Email in use');
     }
     // Hash user pw
-    // 1.
-    // 2.
-    // 3.
+    // 1. Generate salt (16 char long string). toString('hex') returns a Buffer
+    const salt = randomBytes(8).toString('hex');
+    // 2. Hash salt and pw together
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+    // 3. Join the hashed result  and salt together
+    const result = salt + '.' + hash.toString('hex');
     // Create new user and save it
+    const user = await this.userService.create(email, result);
     // return user
+    return user;
   }
 
   signin() {}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,6 +11,7 @@ import {
 } from '@nestjs/common';
 import { Serialize } from 'src/interceptors/serialize.interceptors';
 import { CreateUserDto } from 'src/users/dtos/create-user.dto';
+import { AuthService } from './auth.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UserDto } from './dtos/user.dto';
 import { UsersService } from './users.service';
@@ -18,11 +19,14 @@ import { UsersService } from './users.service';
 @Controller('auth')
 @Serialize(UserDto)
 export class UsersController {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private authService: AuthService,
+  ) {}
 
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    this.usersService.create(body.email, body.password);
+    return this.authService.signup(body.email, body.password);
   }
   @Get('/:id')
   async findUser(@Param('id') id: string) {
