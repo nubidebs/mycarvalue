e36f1f607ee59a3f562e29e30be77e52c817bd3b
nubidebs <debora.crescenzo@truepill.com>
2022-04-14 09:19:54 +0100

    env variables instead of hardcoded cookies

--- src/app.module.ts
+++ src/app.module.ts
@@ -43,11 +43,12 @@ const cookieSession = require('cookie-session');
   ],
 })
 export class AppModule {
+  constructor(private configService: ConfigService) {}
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(
         cookieSession({
-          keys: ['cookieIdWillBeEncrypted'],
+          keys: [this.configService.get('COOKIE_KEY')],
         }),
       )
       .forRoutes('*'); // called every time the app is receiving incoming requests
